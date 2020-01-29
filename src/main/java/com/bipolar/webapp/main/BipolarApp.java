4ba2d3b8f77a615995f308b364c60b0656a501a4
sieogarnie <rowny96@gmail.com>
2020-01-29 20:20:02 +0100

    Initial commit

---
+++ src/main/java/com/bipolar/webapp/main/BipolarApp.java
@@ -0,0 +1,12 @@
+package com.bipolar.webapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipolarApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BipolarApp.class, args);
+    }
+}
