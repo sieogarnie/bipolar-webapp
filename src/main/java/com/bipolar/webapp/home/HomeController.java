69a6542cbaf87ba157d921cff5cfef4a8b0eb8b9
sieogarnie <rowny96@gmail.com>
2020-02-03 23:32:05 +0100

    Add thymeleaf template engine

--- src/main/java/com/bipolar/webapp/home/HomeController.java
+++ src/main/java/com/bipolar/webapp/home/HomeController.java
@@ -2,17 +2,19 @@ package com.bipolar.webapp.home;
 
 import com.bipolar.webapp.services.WebScrapingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @Autowired
     private WebScrapingService webScrapingService;
 
     @RequestMapping(value = {"/", "/home"})
-    public String getHome() {
-        return "home page";
+    public String getHome(Model model) {
+        model.addAttribute("message", "in progress");
+        return "home";
     }
 }
